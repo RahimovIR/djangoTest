947b73bf3d8654ac83b332dcbc3e49da34bf5564
RahimovIR <rahimovir@gmail.com>
2014-06-03 22:23:04 +0600

    change static url settings

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if 'OPENSHIFT_REPO_DIR' in os.environ:
-    STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
+    STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'tables', 'static')
 else:
     STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
